d4a31ece2dbb249c03d5a8975862c6f30b9e9b27
Ali <ali.ali.samadifard@gmail.com>
2025-06-29 13:19:48 +0330

    Add: the rest of the urls and Endpoints

--- Polaris/urls.py
+++ Polaris/urls.py
@@ -1,19 +1,49 @@
 from django.urls import path
-
 from .views import (
-    SignupView, MapDataView, DNSDataView, HTTPResponseView,
-    SMSTestView, PingTestView, DownloadTestView,
-    UploadTestView, MeasuredLatencyView
+    SignupView,
+
+    MapDataCreateView, MapDataListView,
+    DNSDataCreateView, DNSDataListView,
+    HTTPResponseCreateView, HTTPResponseListView,
+    SMSTestCreateView, SMSTestListView,
+    PingTestCreateView, PingTestListView,
+    DownloadTestCreateView, DownloadTestListView,
+    UploadTestCreateView, UploadTestListView,
+    MeasuredLatencyCreateView, MeasuredLatencyListView,
 )
 
 urlpatterns = [
-    path('signup/', SignupView.as_view()),
-    path('mapdata/', MapDataView.as_view()),
-    path('dns/', DNSDataView.as_view()),
-    path('http/', HTTPResponseView.as_view()),
-    path('sms/', SMSTestView.as_view()),
-    path('ping/', PingTestView.as_view()),
-    path('download/', DownloadTestView.as_view()),
-    path('upload/', UploadTestView.as_view()),
-    path('latency/', MeasuredLatencyView.as_view()),
+    path('signup/', SignupView.as_view(), name='signup'),
+
+    # MapData
+    path('mapdata/create/', MapDataCreateView.as_view(), name='mapdata-create'),
+    path('mapdata/', MapDataListView.as_view(), name='mapdata-list'),
+
+    # DNSData
+    path('dnsdata/create/', DNSDataCreateView.as_view(), name='dnsdata-create'),
+    path('dnsdata/', DNSDataListView.as_view(), name='dnsdata-list'),
+
+    # HTTPResponse
+    path('httpresponse/create/', HTTPResponseCreateView.as_view(), name='httpresponse-create'),
+    path('httpresponse/', HTTPResponseListView.as_view(), name='httpresponse-list'),
+
+    # SMSTest
+    path('smstest/create/', SMSTestCreateView.as_view(), name='smstest-create'),
+    path('smstest/', SMSTestListView.as_view(), name='smstest-list'),
+
+    # PingTest
+    path('pingtest/create/', PingTestCreateView.as_view(), name='pingtest-create'),
+    path('pingtest/', PingTestListView.as_view(), name='pingtest-list'),
+
+    # DownloadTest
+    path('downloadtest/create/', DownloadTestCreateView.as_view(), name='downloadtest-create'),
+    path('downloadtest/', DownloadTestListView.as_view(), name='downloadtest-list'),
+
+    # UploadTest
+    path('uploadtest/create/', UploadTestCreateView.as_view(), name='uploadtest-create'),
+    path('uploadtest/', UploadTestListView.as_view(), name='uploadtest-list'),
+
+    # MeasuredLatency
+    path('measuredlatency/create/', MeasuredLatencyCreateView.as_view(), name='measuredlatency-create'),
+    path('measuredlatency/', MeasuredLatencyListView.as_view(), name='measuredlatency-list'),
 ]
