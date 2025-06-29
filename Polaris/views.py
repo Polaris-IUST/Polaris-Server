1827e5d0cbd34d44137bc44fd59d72c3439f972e
Ali <ali.ali.samadifard@gmail.com>
2025-06-29 21:41:46 +0330

    feat: grant all users permission to signup api

--- Polaris/views.py
+++ Polaris/views.py
@@ -1,7 +1,7 @@
 from rest_framework import status, generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from django.contrib.auth import get_user_model
 
 from .serializers import (
@@ -22,6 +22,7 @@ class SignupView(APIView):
     POST /signup/
     Creates a new user account.
     """
+    permission_classes = [AllowAny]
     def post(self, request):
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
@@ -37,7 +38,6 @@ class BaseDataCreateView(APIView):
     - serializer_class
     - model_class (optional for further customization)
     """
-    permission_classes = [IsAuthenticated]
     serializer_class = None
     model_class = None
 
